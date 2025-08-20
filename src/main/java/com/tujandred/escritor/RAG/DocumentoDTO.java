970fdf90697a539d400949a212d3e7a0d03cad07
tujandred <1098512+tujandred@users.noreply.github.com>
2025-08-20 19:19:45 +0200

    Creación de DocumentoDTO

---
+++ src/main/java/com/tujandred/escritor/RAG/DocumentoDTO.java
@@ -0,0 +1,17 @@
+package com.tujandred.escritor.RAG;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocumentoDTO {
+    private String id;
+    private String nombre;
+    private String contenido;
+    private Map<String, Object> metadatos;
+}
