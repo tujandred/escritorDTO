b50b25b919b3b53efcb55bb569a96743b48d0fe6
tujandred <1098512+tujandred@users.noreply.github.com>
2025-08-20 15:11:32 +0200

    Inclusión NodoJerarquiaDto

---
+++ src/main/java/com/tujandred/escritor/jerarquia/NodoJerarquiaDto.java
@@ -0,0 +1,48 @@
+package com.tujandred.escritor.jerarquia;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Schema(name = "NodoJerarquiaDto", description = "Nodo de la jerarquía de entidades. Contiene información sobre la entidad, su relación jerárquica y sus hijos.")
+public class NodoJerarquiaDto implements Serializable {
+    @Schema(description = "Identificador único del nodo", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+    @Schema(description = "Nombre del nodo", example = "Capítulo 1", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String nombre;
+    @Schema(description = "Descripción del nodo", example = "Primer capítulo de la historia", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String descripcion;
+    @Schema(description = "Número de orden dentro de su jerarquía. Si es menor o igual que 0 o null, indica no ordenado", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Integer orden;
+    @Schema(description = "Término de búsqueda asociado", example = "capitulo1", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String searchTerm;
+    @Schema(description = "Etiqueta del nodo", example = "Capítulo", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String etiqueta;
+    @Schema(description = "Tipo de datos del nodo", example = "Texto", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String tipoDatos;
+    @Schema(description = "ID del nodo padre", example = "50", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long padreId;
+    @Schema(description = "Lista de nodos hijos", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private List<NodoJerarquiaDto> hijos = new ArrayList<>();
+    @Schema(description = "Lista de tipos asociados al nodo", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private List<String> tipos = new ArrayList<>();
+}
